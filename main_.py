80bdcdf90f22e7ae8b0a592d144dd90c2a40579c
Nat4Lia <33526078+Nat4Lia@users.noreply.github.com>
2019-09-23 13:33:21 +0800

    Update main_.py

--- main_.py
+++ main_.py
@@ -29,8 +29,8 @@ if __name__ == '__main__' :
     ping_ip(ip_address)
     save_macaddress()
     checking_table()
-    try_update()
     if not list_used_ip_fp :
+        try_update()
         lcd_.teks(text1='TIDAK ADA',text2='FINGERPRINT',text3='YANG TERHUBUNG')
         time.sleep(1.2)
         lcd_.teks(text1='HARAP HUBUNGKAN',text2='RASPBERRY',text3='KE FINGERPRINT')
